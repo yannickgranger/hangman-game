7895e7cc9a6e2dc139621a171467e31d5eb25846
yannick <yannick.gger@gmail.com>
2024-05-01 23:16:26 +0200

    create routes

---
+++ src/UI/Http/CreateGameAction.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\UI\Http;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route(path: "game", name: "start_game", methods: ["POST"])]
+class CreateGameAction
+{
+    public function __invoke(Request $request): JsonResponse
+    {
+        return new JsonResponse();
+    }
+}
---
+++ src/UI/Http/GuessLetterAction.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\UI\Http;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route(path: "game/guess",name: "game_guess_letter", methods: ["POST"])]
+class GuessLetterAction
+{
+    public function __invoke(Request $request): JsonResponse
+    {
+        return new JsonResponse();
+    }
+}
